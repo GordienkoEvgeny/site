da02e2ee7fb14e97af3984a870b90432ede30c23
GordienkoEvgeny <evgeny.gordienko95@gmail.com>
2023-05-04 00:30:04 +0300

    fixed bug with hidden password

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -11,7 +11,7 @@ const keysTranslation = {
 };
 
 const Input = ({ formik, type, focused }) => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
   const inputRef = useRef();
   const { t } = useTranslation();
   useEffect(() => {
@@ -45,7 +45,7 @@ const Input = ({ formik, type, focused }) => {
         onChange={formik.handleChange}
         placeholder={t(keysTranslation[type])}
         ref={inputRef}
-        type={(type === 'password' && showPassword) || (type === 'passwordConfirmation' && showPassword) ? 'text' : 'password'}
+        type={!(type === 'password' && showPassword) || (type === 'passwordConfirmation' && showPassword) ? 'text' : 'password'}
       />
       {type === 'password' || type === 'passwordConfirmation' ? (
         <div className="button-showPass-box">
